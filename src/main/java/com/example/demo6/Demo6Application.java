63da2a00253e3cb96488908da6506c0305dda665
shreySaxena <shreyyerhs07@gmail.com>
2023-12-01 18:54:06 +0530

    added comments to explain

--- src/main/java/com/example/demo6/Demo6Application.java
+++ src/main/java/com/example/demo6/Demo6Application.java
@@ -17,6 +17,8 @@ public class Demo6Application {
 
 
 		//CREATE
+		//UserRespository interface has all methods to interact with database, we dont need to explicitly define methods in userRepository,
+        // we just need to extendit
 		User user=new User();
 		user.setName("shrey");
 		user.setCity("jabalpur");
@@ -28,16 +30,20 @@ public class Demo6Application {
         //this save method saves only one object, if you want to save a List of students then you can use saveAll() method
 
 		//READ
+		//we got row by giving its id(@Id attribute in User class).And that whole row's columns have been converted to an User object with respective attributes
 		Optional<User> optional=userRepository.findById(2);
 		User user2=optional.get();
 		System.out.println(user2);
+		//this user2 has id 2 and other attributes
 
 		//UPDATE
+		//because user2 was having id 2, so  row which has id=2 has been updated
 		user2.setName("shrey saxena updated");
 		userRepository.save(user2);
 		System.out.println(user2);
 
 		//DELETE
+       //because user2 was having id 2, so  row which has id=2 has been deleted
 		userRepository.delete(user2);
 
 		//we can also use deleteById()
